873d766eb29287f84643b7a0e9ca674bcdef2717
Ming Zhang <mingzhang@Mings-MacBook-Pro.local>
2022-04-18 17:02:29 -0400

    synchronize with Iteration4 update

--- code/backend/code/myapp/tests/test_recommend_project.py
+++ code/backend/code/myapp/tests/test_recommend_project.py
@@ -15,33 +15,12 @@ class test_recommendProject(TestCase):
         print( 80*"-" + "\nPass setUp")
                  
     def test_returnAllProject(self):
-        url = 'http://127.0.0.1:8000/create_project_api'
-        print(80*"-" + "\nstart to create project")
-        print(80*"-" + self.user.name)
-        self.client = Client()
-        json_data = {
-                        "HTTP_X_TOKEN": "-1",
-                        "projectName": "projectName",
-                        "teamName" : "teamName",
-                        "project_idea" : "project_idea",
-                        "projectIntroduction" : "projectIntroduction",
-                        "intendedLanguage" : ["c++", "java"],
-                        "otherLanguage" : "otherLanguage",
-                        "skillWanted": "skillWanted",
-                        "keywords": "keywords"
-                    }
-        self.response = self.client.post(url, json.dumps(json_data), content_type="application/json")
-        return_json = json.loads(self.response.content)
-        print(return_json['msg'])
-
         url_queryProject = 'http://127.0.0.1:8000/recommend_project_api'
-        print(80*"-" + "\nstart to recommend project")
-        print(80*"-" + self.user.name)
-        json_data_queryProject = {
-                        "HTTP_X_TOKEN": "-1"
+        client = Client()
+        print(80*"-" + "\nTest login with created account")
+        json_data = {
+                    "HTTP_X_TOKEN" : "-1",
                     }
-        self.response = self.client.get(url_queryProject, json.dumps(json_data_queryProject), content_type="application/json")
-        return_json = json.loads(self.response.content)
-        print(return_json)
-
+        response_json = post_request(client, "recommend_project_api", json_data)
+        return response_json
 
